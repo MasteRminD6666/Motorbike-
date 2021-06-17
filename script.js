a020e6b36f3f7f47653be518720aff37f3c083b2
MasteRminD <rami-zaitoun99@hotmail.com>
2021-06-17 13:28:23 +0300

    Valdation added

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ about.addEventListener('click' ,function(){
 
 Welcome()
 
-console.log(motortype)
+
 
 const times = () =>{
     alert("how many time you want that pic to show up ")
@@ -58,8 +58,10 @@ const times = () =>{
      }
         else{
              for (let number = 1; number<value; number++){
+                 console.log(number)
                 if( motortype == 1){
-      document.write('<img src="https://cdn.shopify.com/s/files/1/0965/7500/products/RKv9re4_1024x1024_82671096-ec81-4884-a309-6bb8147a295e.jpg?v=1553829485">')
+      document.write('<img src="https://www.bennetts.co.uk/-/media/bikesocial/2020-june-images/motorcycle-types-guide/harley_fat_boy_cruiser.ashx?h=493&w=740&la=en&hash=070B31B0992716E1ED7953C1252CBF00ECE7C551">')
+      
 
     }
     if (motortype ==2 ){
